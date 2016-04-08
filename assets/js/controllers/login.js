70c60cc9b82d64b949c4e9e1b0516dee647c0069
AlaynF <alayn@iMac.local>
2016-04-08 18:49:23 -0400

    key[ad

--- assets/js/controllers/login.js
+++ assets/js/controllers/login.js
@@ -8,4 +8,23 @@ function (vm, http, $timeout, $window, root) {
 	document.title = 'Login - MBD Repair Tools';
 
 	root.$broadcast('changeTitle', {title: 'Login'});
+
+	vm.passcode = '';
+
+    vm.keypadclick = function(number) {
+        if (!number) {
+            $scope.passcode = '';
+            return;
+        }
+
+        vm.passcode += number;
+
+        if (vm.passcode == "12345") {
+            $window.location.href  = "/register";
+        }
+
+        if  (vm.passcode.length > 5) {
+            alert("Password too long");
+        }
+    }
 }]);
